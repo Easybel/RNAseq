2f967c36823eda36f32c24997d0c17b094dfc9cf
Easybel <57913876+Easybel@users.noreply.github.com>
2020-01-13 17:59:11 +0100

    Update 3_reads2Tab.sh

--- 3_reads2Tab.sh
+++ 3_reads2Tab.sh
@@ -1,6 +1,6 @@
 #Here, we first paste all the files next to each other, then we print (awk) the columns we want 
 IDwt="SS_root_wt_"
-IDCu="SS_root_wt_"
+IDCu="SS_root_Cu_"
 end="_raw.count"
 IDout="myTab"
 paste -d' ' $IDwt"s1"$end $IDwt"s2"$end $IDwt"s3"$end $IDCu"s1"$end $IDCu"s2"$end $IDCu"s3"$end| grep -v "^#" | awk '{print $1,$7,$14,$21,$28,$35,$42}' > $IDout".csv"

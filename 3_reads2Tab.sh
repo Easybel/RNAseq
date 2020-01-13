2c37b861a49396cde38f941f6354d8ffb310f0f8
Easybel <57913876+Easybel@users.noreply.github.com>
2020-01-13 15:41:28 +0100

    Create 3_reads2Tab.sh

---
+++ 3_reads2Tab.sh
@@ -0,0 +1,6 @@
+#Here, we first paste all the 
+IDwt="SS_root_wt_"
+IDCu="SS_root_wt_"
+end="_raw.count"
+IDout="myTab"
+paste -d' ' $IDwt"s1"$end $IDwt"s2"$end $IDwt"s3"$end $IDCu"s1"$end $IDCu"s2"$end $IDCu"s3"$end| grep -v "^#" | awk '{print $1,$7,$14,$21,$28,$35,$42}' > $Idout".csv"

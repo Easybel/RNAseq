8cd2722ae08418fb866f66424e50d7becfce3a92
Easybel <57913876+Easybel@users.noreply.github.com>
2020-01-13 15:50:52 +0100

    Update 3_reads2Tab.sh

--- 3_reads2Tab.sh
+++ 3_reads2Tab.sh
@@ -1,4 +1,5 @@
-#Here, we first paste all the 
+#Here, we first paste all the files next to each other, then we print (awk) the columns we want and in the last step we 
+#remove the first line (tail) 
 IDwt="SS_root_wt_"
 IDCu="SS_root_wt_"
 end="_raw.count"

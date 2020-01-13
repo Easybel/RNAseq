1652c2d29c3278ee0aa18d4b7cf4a8509d32f00f
Easybel <57913876+Easybel@users.noreply.github.com>
2020-01-13 15:49:09 +0100

    Update 3_reads2Tab.sh

--- 3_reads2Tab.sh
+++ 3_reads2Tab.sh
@@ -3,4 +3,6 @@ IDwt="SS_root_wt_"
 IDCu="SS_root_wt_"
 end="_raw.count"
 IDout="myTab"
-paste -d' ' $IDwt"s1"$end $IDwt"s2"$end $IDwt"s3"$end $IDCu"s1"$end $IDCu"s2"$end $IDCu"s3"$end| grep -v "^#" | awk '{print $1,$7,$14,$21,$28,$35,$42}' > $Idout".csv"
+paste -d' ' $IDwt"s1"$end $IDwt"s2"$end $IDwt"s3"$end $IDCu"s1"$end $IDCu"s2"$end $IDCu"s3"$end| grep -v "^#" | awk '{print $1,$7,$14,$21,$28,$35,$42}' | tail -n +2 > $IDout".csv"
+ 
+

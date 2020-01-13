8eb27ac3f6d632260087b6294c99b1399291a4ef
Easybel <57913876+Easybel@users.noreply.github.com>
2020-01-13 17:54:38 +0100

    Update 3_reads2Tab.sh

--- 3_reads2Tab.sh
+++ 3_reads2Tab.sh
@@ -1,9 +1,11 @@
-#Here, we first paste all the files next to each other, then we print (awk) the columns we want and in the last step we 
-#remove the first line (tail) 
+#Here, we first paste all the files next to each other, then we print (awk) the columns we want 
 IDwt="SS_root_wt_"
 IDCu="SS_root_wt_"
 end="_raw.count"
 IDout="myTab"
-paste -d' ' $IDwt"s1"$end $IDwt"s2"$end $IDwt"s3"$end $IDCu"s1"$end $IDCu"s2"$end $IDCu"s3"$end| grep -v "^#" | awk '{print $1,$7,$14,$21,$28,$35,$42}' | tail -n +2 > $IDout".csv"
- 
+paste -d' ' $IDwt"s1"$end $IDwt"s2"$end $IDwt"s3"$end $IDCu"s1"$end $IDCu"s2"$end $IDCu"s3"$end| grep -v "^#" | awk '{print $1,$7,$14,$21,$28,$35,$42}' > $IDout".csv"
+
+#optionally: we remove the first line (tail) 
+#paste -d' ' $IDwt"s1"$end $IDwt"s2"$end $IDwt"s3"$end $IDCu"s1"$end $IDCu"s2"$end $IDCu"s3"$end| grep -v "^#" | awk '{print $1,$7,$14,$21,$28,$35,$42}' | tail -n +2 > $IDout".csv"
+
 

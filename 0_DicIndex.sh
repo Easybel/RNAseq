484e384af6f5c5cbe0f32adae99ea7a9ad8a70b1
Easybel <57913876+Easybel@users.noreply.github.com>
2020-09-01 20:10:52 +0200

    Update 0_DicIndex.sh

--- 0_DicIndex.sh
+++ 0_DicIndex.sh
@@ -1,28 +1,16 @@
-#specify the software that is needed, the path to the dict and the name if the dict
-#Define input and output paths
-#In the myQCPath there should be a folder called FastQC_AfterTrimm, where the trimmed QC of the results will be put
+# Here, the fasta will be indexed for the Star mapping.
+# Define the paths to the dictionary (this is also the output path) and its name. 
+# Also provide the path to STAR and 
 
-myDictPath="/home/irathman/scripts/scripts_git/test"
+myDictPath="/projects/ag-advol/dictionaries/MS11_ncbi_IR" #!
 
+# name of dictionary
+dict="MS11"
+ann="MS11"
 
 #Define folders where software is installed
+StarFold="/home/irathman/sw/STAR-2.5.3a/bin/Linux_x86_64"
 
-bwaFold="/home/irathman/sw/bwa-0.7.17"
-samFold="/home/irathman/sw/samtools-1.8"
-picardFold="/home/irathman/sw"
-#GATKFold="/home/irathman/sw/GATK3.5"
-#bcfFold="/home/irathman/sw/bcftools-1.8"
-
-# here you map against: .fasta
-dict="BsubNC_000964wt"
-
-cd $bwaFold
-./bwa index $myDictPath/$dict.fasta
-
-module unload openjdk/1.8.0_202
-module load openjdk/1.8.0_60
-cd $picardFold
-java -Xms1g -Xmx3g -jar picard.jar CreateSequenceDictionary R=$myDictPath/$dict.fasta O=$myDictPath/$dict.dict
-
-cd $samFold
-./samtools faidx $myDictPath/$dict.fasta
+cd $StarFold
+./STAR --runThreadN 8 --runMode genomeGenerate --genomeDir $myDictPath --genomeFastaFiles $myDictPath/$dict".fasta" \
+--sjdbGTFtagExonParentTranscript Parent --sjdbGTFfile $myDictPath/$ann".gff3" --sjdbOverhang 149

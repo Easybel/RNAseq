c0d647c79e357d9d00a028007df45b456592322d
Easybel <57913876+Easybel@users.noreply.github.com>
2020-09-02 08:33:54 +0200

    Update 0_DicIndex.sh

--- 0_DicIndex.sh
+++ 0_DicIndex.sh
@@ -5,12 +5,21 @@
 myDictPath="/projects/ag-advol/dictionaries/MS11_ncbi_IR" #!
 
 # name of dictionary
-dict="MS11"
-ann="MS11"
+dict="MS11"  #!
+ann="MS11" #!
 
 #Define folders where software is installed
-StarFold="/home/irathman/sw/STAR-2.5.3a/bin/Linux_x86_64"
+StarFold="/home/irathman/sw/STAR-2.5.3a/bin/Linux_x86_64" #!
+
+#The basic options to generate genome indices with STAR are as follows:
+#--runThreadN NumberOfThreads
+#--runMode genomeGenerate
+#--genomeDir /path/to/genomeDir
+#--genomeFastaFiles /path/to/genome/fasta1 /path/to/genome/fasta2 ...
+#--sjdbGTFfile /path/to/annotations.gtf
+#--sjdbOverhang ReadLength-1
+# !!--genomeSAindexNbases min(14, log2(GenomeLength)/2 - 1) (For small genomes, this parameter must be scaled down)!!
 
 cd $StarFold
 ./STAR --runThreadN 8 --runMode genomeGenerate --genomeDir $myDictPath --genomeFastaFiles $myDictPath/$dict".fasta" \
---sjdbGTFtagExonParentTranscript Parent --sjdbGTFfile $myDictPath/$ann".gff3" --sjdbOverhang 149
+--sjdbGTFtagExonParentTranscript Parent --sjdbGTFfile $myDictPath/$ann".gff3" --sjdbOverhang 149 

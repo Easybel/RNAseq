b3f580a302627cf359cc6d96b9e35a8111694142
Easybel <57913876+Easybel@users.noreply.github.com>
2020-09-17 18:37:14 +0200

    Update 2_CombiScriptStar.sh

--- 2_CombiScriptStar.sh
+++ 2_CombiScriptStar.sh
@@ -28,7 +28,7 @@ dict="MS11"
 ID=$(ls -1 $myDataTrim | grep "_1P.fastq" | sed -n ''$i'p' | cut -d"_" -f1,2,3,4)
 IDout=$(echo $ID)
 
-# Mapping the reads to the reference with STAR
+####### Mapping the reads to the reference with STAR
 # basic options:
 #--runThreadN NumberOfThreads
 #--genomeDir /path/to/genomeDir
@@ -44,19 +44,28 @@ cd $StarFold
 --readFilesIn $myDataTrim/$ID"_1P.fastq" $myDataTrim/$ID"_2P.fastq" 
 echo "done with mapping"
 
-
+##### data cpnverted
 #..sam is converted here to bam, sorted and indexed, so that it can be oppened with IGVviewer
 cd $samFold
 ./samtools view -b $myDataPath/$IDout"_Aligned.out.sam" --threads 8 -T $myDictPath/$dict.fasta -o $myDataPath/$IDout".bam"
 ./samtools sort $myDataPath/$IDout".bam" --threads 8 --reference $myDictPath/$dict.fasta -o $myDataPath/$IDout"_sort.bam"
 ./samtools index -b $myDataPath/$IDout"_sort.bam" > $myDataPath/$IDout"_sort.bam.bai"
 
-# Read quantification
+###### FeatureCounts: Read quantification
 # Here the featurecount is done with subread package -> output: count table
 # Input data (i) aligned reads in sam/bam, (ii) list of genomic features in either GTF, GFF or simplified annotation format (SAF)
+# Options:
+# -t which feature Type to count, e.g. "exon", "CDS"
+# -p : for paired reads and -f for counting fragments instead of reads
+# -M count multimappers each as 1 count or with --fraction 
+# -O count multi-overlapping reads each as 1 count or with --fraction; --minOverlap give the number of bp that overlap has to be long
 
 cd $featureCounts
-./featureCounts -p -T 8 -F GTF -a $myDictPath/$dict".gft" $myDataPath/$IDout"_Aligned.out".sam \
--o $myDataPath/$IDout"_raw.count"
+# change the output name depending on settings
+IDout2=$IDout"_O_minO20"
+./featureCounts -p -O --minOverlap 20 -t "CDS" -T 8 -F GTF -a $myDictPath/$dict".gft" $myDataPath/$IDout"_Aligned.out".sam \
+-o $myDataPath/$IDout2"_CDS_raw.count"
+./featureCounts -p -O --minOverlap 20 -t "exon" -T 8 -F GTF -a $myDictPath/$dict".gft" $myDataPath/$IDout"_Aligned.out".sam \
+-o $myDataPath/$IDout2"_exon_raw.count"
 #done
 exit 0
